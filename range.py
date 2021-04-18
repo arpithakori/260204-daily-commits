7784ee352bb787798eb347949dbd21819b3fd438
Arpitha S Kori <arpitha.ssaa@gmail.com>
2021-04-19 01:22:54 +0530

    ranges commit-pycharm win env

---
+++ range.py
@@ -0,0 +1,15 @@
+# range version 1
+num = list(range(10))
+print(num)
+
+# range version 2
+num_1 = list(range(3, 8))
+print(num_1)
+
+# range version 3
+num_2 = list(range(5, 20, 2))
+print(num_2)
+
+# range version 4
+num_3 = list(range(20, 5, -2))
+print(num_3)
