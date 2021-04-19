cc2756e2c3f5e77c228a45dc61a21bb239fdd844
Arpitha S Kori <arpitha.ssaa@gmail.com>
2021-04-19 22:09:13 +0530

    strings commit-pycharm win env

---
+++ stringfun.py
@@ -0,0 +1,20 @@
+# join
+print(", ".join(["spam", "eggs", "ham"]))
+
+# replace
+print("Hello Me".replace("Me", "World"))
+
+# startswith
+print("This is a sentence.".startswith("This"))
+
+# endswith
+print("This is a sentence.".endswith("sentence."))
+
+# upper
+print("This is a sentence".upper())
+
+# lower
+print("I LOVE INDIA".lower())
+
+# split
+print("spam,eggs,ham".split(","))
