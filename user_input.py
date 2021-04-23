67bde1fb0df5646a2d6e2bb7e72b23163cb11014
Arpitha S Kori <arpitha.ssaa@gmail.com>
2021-04-23 08:46:06 +0530

    assignment commits-pycharm win env

---
+++ user_input.py
@@ -0,0 +1,11 @@
+user_input = input("Enter the text => lol, rofl, lmk, smh:\n")
+if user_input == "lol":
+    print("laughing out loud")
+elif user_input == "rofl":
+    print("rolling on the floor")
+elif user_input == "lmk":
+    print("let me know")
+elif user_input == "smh":
+    print("shake my head")
+else:
+    print("Invalid input")
