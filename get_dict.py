da678f0ebb7000718d016e0530afd61b3cc1ff94
Arpitha S Kori <arpitha.ssaa@gmail.com>
2021-04-26 01:11:07 +0530

    dictionary (get) commit-pycharm win env

---
+++ get_dict.py
@@ -0,0 +1,7 @@
+pairs = {1:"apples",
+         "orange":[2,3,4],
+         True:False,
+         None:"True",}
+print(pairs.get("orange"))
+print(pairs.get(7))
+print(pairs.get(12345,"not in dictionary"))
